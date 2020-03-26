864573e12788a6977fffa9bb1791089f3c410236
Nischal Kanishk <nischalkanishk@gmail.com>
2020-03-26 10:06:44 +0530

    Version 0.0.1

---
+++ models/User.js
@@ -0,0 +1,25 @@
+const mongooose = require('mongoose')
+const UserSchema = new mongooose.Schema({
+    name:{
+        type:String,
+        required: true
+    },
+    email: {
+        type: String,
+        required:true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = User = mongooose.model('user', UserSchema)
--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000
+const connectDB = require('./config/db')
 
+connectDB()
 
 app.get('/', (req, res) => {
     res.send('hellp')
 })
+
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
 
 app.listen(PORT, () => console.log(`Server started at ${PORT}`))
